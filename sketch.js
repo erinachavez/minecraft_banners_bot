6bc4ec7b0ec60178aa0a3ee3342a91bacb4c4172
Erina Chavez <chavez.erina@gmail.com>
2021-02-28 13:57:35 -0500

    remove port #

--- sketch.js
+++ sketch.js
@@ -102,6 +102,6 @@ module.exports = {
     p5.createSketch(sketch);
   },
   setAppPort: function(port) {
-    instance = axios.create({ baseURL: `https://minecraft-banners-bot.herokuapp.com:${port}`});
+    instance = axios.create({ baseURL: `https://minecraft-banners-bot.herokuapp.com`});
   }
 }
